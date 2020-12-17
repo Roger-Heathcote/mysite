8f0eb532e7677d262e91d1f9173892312f33c8b8
Roger-Heathcote <github@technicalbloke.com>
2020-12-17 21:26:11 +0000

    Added more themes

--- backdrop.js
+++ backdrop.js
@@ -47,7 +47,7 @@ const rootElem = document.querySelector("body")
 
 // Scene
 const NEAR = 0.2
-const FAR = 95
+const FAR = 80
 
 const BOX_COL = 0xff5f00;
 const GRID_COL = 0xff5f00;
@@ -68,31 +68,27 @@ const renderer = new THREE.WebGLRenderer( { antialias: true } )
 renderer.setPixelRatio( window.devicePixelRatio )
 renderer.setSize(window.innerWidth, window.innerHeight)
 rootElem.appendChild(renderer.domElement)
+const canvas = document.querySelector("canvas")
+canvas.setAttribute('alt', '3D animated backdrop - decorative - flying over an abstract geometrical plane, through a cloud of spinning cuboids.')
 window.addEventListener( 'resize', onWindowResize, false );
 
-// Glow effect
-// const composer = new EffectComposer( renderer );
-
-
-
-
 
 // OBJECTS
 // OBJECTS
 // OBJECTS
 
-// PLANE
+// Plane
 var planeGeo = new THREE.PlaneBufferGeometry(100, 100, 8, 8);
-var planeMat = new THREE.MeshBasicMaterial({ color: 0xf0f000, side: THREE.DoubleSide });
-var plane = new THREE.Mesh(planeGeo, planeMat);
-plane.rotateX( - Math.PI / 2);
+var planeMat = new THREE.MeshBasicMaterial({ color: 0xf0f000, side: THREE.DoubleSide })
+var plane = new THREE.Mesh(planeGeo, planeMat)
+plane.rotateX( - Math.PI / 2)
 plane.translateY(-1)
 plane.material.transparent = true
 plane.material.opacity = 0
 scene.add(plane);
 
-
-// GRID
+// Grid
+// threejs grid helper had color matching issues so we hand roll a grid. 
 const size = 100;
 const gridPoints = []
 const lineStart = -(size/2)
@@ -111,25 +107,28 @@ const gridGeo = new THREE.BufferGeometry().setFromPoints( gridPoints )
 const theGrid = new THREE.Line( gridGeo, new THREE.LineBasicMaterial({ color: GRID_COL }) )
 scene.add(theGrid)
 
-
-
-// CENTER LINE
+// Center line - needed to prevent flicker when plane is visible
 const centerLinePoints = [ new THREE.Vector3(0, 0, -50), new THREE.Vector3(0, 0.1, 50) ]
 const centerLineGeo = new THREE.BufferGeometry().setFromPoints( centerLinePoints )
 const centerLine = new THREE.Line( centerLineGeo, new THREE.LineBasicMaterial({ color: GRID_COL }) )
 scene.add(centerLine)
 
-
-// CREATE RECTANGLES
+// 200 Cuboids
 const rects = []
 const _ = [... Array(200)].map(()=>{
 
+	// Set distance - cuboids start a random disance behind the fog
 	const dist = randIntRange(FAR,FAR*2)
-	const xTrans = randIntRange(-4*(dist/3)/3,4*(dist/3)/3)
-	const yTrans = randIntRange(2,2*(dist/3)/4)
-	if(xTrans < 3 && xTrans > -3 && yTrans < 5) return (console.log("Skipped one"))
 
-	// console.log("Making rects")
+	// Set how far left/right the cuboids are
+	const xTrans = randIntRange(-4*(dist/3)/3,4*(dist/3)/3)
+
+	// Set height - Minimum distance from plane/grid is 2
+	const yTrans = randIntRange(2,2*(dist/3)/4)
+
+	// Abort if the cuboid is on a collision course with the camera
+	if(xTrans < 3 && xTrans > -3 && yTrans < 5) return
+
 	const boxGeometry = new THREE.BoxGeometry(
 		randIntRange(10,50) / 15,
 		randIntRange(10,50) / 15,
@@ -137,30 +136,30 @@ const _ = [... Array(200)].map(()=>{
 	)
 	boxGeometry.center()
 	
+	// Make solid cuboid
+	// We shrink it bit so it doesn't fight with the wireframe 
 	const boxMaterial = new THREE.MeshBasicMaterial({
-		color: 0x111111,
 		polygonOffset: true,
-		polygonOffsetFactor: 1, // positive value pushes polygon further away
+		polygonOffsetFactor: 1, // positive value push polygon further away
 		polygonOffsetUnits: 1
 	})
 	const cube = new THREE.Mesh( boxGeometry, boxMaterial )
 
-	// const lineColor = new THREE.Color(`hsl(${randIntRange(0,359)}, 100%, 50%)`)
-
-	const geo = new THREE.EdgesGeometry( cube.geometry ) // or WireframeGeometry
-	const mat = new THREE.LineBasicMaterial( { color: BOX_COL  } )
-	const wireframe = new THREE.LineSegments( geo, mat )
+	// Make wireframe and attach it to cuboid
+	const wfGeo = new THREE.EdgesGeometry( cube.geometry ) // or WireframeGeometry
+	const wfMat = new THREE.LineBasicMaterial( { color: BOX_COL  } )
+	const wireframe = new THREE.LineSegments( wfGeo, wfMat )
 	cube.add( wireframe )
 
-	// Move them about
+	// Move cuboids into place
 	cube.translateZ(-dist)
 	cube.translateX(xTrans)
 	cube.translateY(yTrans)
-
 	cube.rotation.x = Math.PI * randIntRange(10,50) / 10 
 	cube.rotation.y = Math.PI * randIntRange(10,50) / 10 
 	cube.rotation.z = Math.PI * randIntRange(10,50) / 10 
 
+	// Push cuboid, wireframe onto list and give them rotation and speed factors
 	rects.push({
 		object: cube,
 		wireframe: wireframe,
